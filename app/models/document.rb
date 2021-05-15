60aee24b09bf6e2d490e7695cc5bf9bf64d1b673
Ravi Singh <ravi@appsimpact.com>
2021-05-15 11:41:54 +0530

    worked on document resource

---
+++ app/models/document.rb
@@ -0,0 +1,6 @@
+class Document < ApplicationRecord
+  validates :title, :description, presence: true
+  # Active storage attachments
+  has_one_attached :doc_file
+  has_many_attached :doc_file_pages
+end
